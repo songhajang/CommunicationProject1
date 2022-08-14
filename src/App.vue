39d02d859dca0cd38bf4120dea85c06b68e53a82
songhajang <angpan2004@gmail.com>
2022-08-14 15:37:15 +0900

    기초 설정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" class="text-red-500"/>
+  </div>
 </template>
 
 <script>
