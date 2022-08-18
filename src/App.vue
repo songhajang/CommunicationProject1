aa8e1f66340f7f58e93d0c8645f2d3c1fcf289d5
songhajang <angpan2004@gmail.com>
2022-08-18 11:09:40 +0900

    v-modal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" class="text-red-500"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
